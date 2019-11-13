cb971490184dc1581cb7e605881de03bd09fde97
naflav <55958629+naflav@users.noreply.github.com>
2019-11-13 01:04:36 +0100

    Finished assignment

--- sketch.js
+++ sketch.js
@@ -1,11 +1,41 @@
-function preload(){
+function preload() {
   // put preload code here
 }
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background('#9F7CE4');
+
+  //question definition
+  var myText = "Did I forget my drawing tablet back in Brasil";
+  textFont("Fondamento");
+  textSize(60);
+  fill('#333');
+  textAlign(CENTER);
+  text(myText, width / 2, height / 2);
+
+  //answer definition
+  var myText = "click anywhere to remember";
+  textFont("Source Code Pro");
+  textSize(16);
+  fill('#333');
+  textAlign(CENTER);
+  text(myText, width / 2, height / 2 + 60);
+
 }
 
 function draw() {
   // put drawing code here
+
 }
+
+
+function mouseClicked() {
+  window.open('index2.html', '_self');
+}
+
+//this is to redirect to another page
+//function mouseClicked () {
+//  window.open('https://www.wikihow.com/Make-a-Chair', '_self')
+//}
---
+++ sketch2.js
@@ -0,0 +1,80 @@
+let myImage;
+var myMap;
+var canvas;
+var key = "pk.eyJ1IjoiYW5mbCIsImEiOiJjazJuMXQzdGYwaW12M2JuenpiYjFha2FjIn0.96lS6KE5xgkDatYk7N-XgA";
+var mappa = new Mappa("MapboxGL", key);
+var homeLat = -12.8752949;
+var homeLon = -38.5716907;
+var meLat = 45.5048151;
+var meLon = 9.1629839;
+var options = {
+  lat: 0,
+  lng: 0,
+  zoom: 2,
+  style: "mapbox://styles/mapbox/dark-v9"
+}
+
+
+
+
+
+function preload() {
+  myImage = loadImage("tablet.png");
+  myImage2 = loadImage("myself.png");
+}
+
+
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
+}
+
+
+
+function draw() {
+  // put drawing code here
+  clear();
+
+  var salvador = myMap.latLngToPixel(homeLat, homeLon);
+  var me = myMap.latLngToPixel(meLat, meLon);
+
+  //Line
+  line(salvador.x, salvador.y, me.x, me.y);
+
+  //tablet location
+  ellipse(salvador.x, salvador.y, 5);
+  image(myImage, salvador.x, salvador.y, myImage.width = 50, myImage.height = 50);
+
+  //my location
+  image(myImage2, me.x, me.y, myImage2.width = 50, myImage2.height = 50);
+  ellipse(me.x, me.y, 5);
+  stroke("#9F7CE4");
+  strokeWeight(2);
+
+  //title definitions
+  push();
+  var myText = "Yeah I did";
+  textFont("Fondamento");
+  textSize(60);
+  fill('#9F7CE4');
+  noStroke();
+  textAlign(CENTER);
+  text(myText, 200, 100);
+  pop();
+
+  //text definition
+  push();
+  var myText = "god damn";
+  textFont("Source Code Pro");
+  textSize(20);
+  fill('#9F7CE4');
+  noStroke();
+  textAlign(CENTER);
+  text(myText, 200 - 70, 100 + 50);
+  pop();
+
+
+}
